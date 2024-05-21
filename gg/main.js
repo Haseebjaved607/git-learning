31eedbe4f3661a8cf945caa65c96746db1279ec9
Hasiimughal <137723481+Hasiimughal@users.noreply.github.com>
2024-05-22 00:17:26 +0500

    nothing

--- gg/main.js
+++ gg/main.js
@@ -1,1 +1,8 @@
-console.log("helo")
+const http = require("http")
+
+const myServer = http.createServer((req , res) =>{
+    res.end("server run")
+
+});
+ 
+myServer.listen(5000,()=>console.log("run ho gaya ") ) 
